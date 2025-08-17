b222ca836b58a52b1f9a13130f69f5f276332132
neilrocks <swapnilroy22@gmail.com>
2025-08-17 15:46:37 +0530

    Connected client with local peer js server at port 3000

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -21,7 +21,11 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
     }
     useEffect(()=>{
         const userId = UUIDv4();
-        const newPeer = new Peer(userId);
+        const newPeer = new Peer(userId,{
+            host: 'localhost',
+            port: 3000,
+            path: '/myapp'
+        });
         setUser(newPeer);
         fetchUserFeed();
         const enterRoom=({roomId}:{roomId:string})=>{
